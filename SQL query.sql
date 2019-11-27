7b633cc56cae4fe22a0c78e565a3884f19a26da6
chaochaoachao <44150379+chaochaoachao@users.noreply.github.com>
2019-11-27 18:29:58 -0500

    Update SQL query.sql

--- SQL query.sql
+++ SQL query.sql
@@ -5,40 +5,6 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept-emt" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
-    "from_date" timestamp   NOT NULL,
-    "to_date" timestamp   NOT NULL
-);
-
-CREATE TABLE "dept-manager" (
-    "dept_no" varchar   NOT NULL,
-	"emp_no" int   NOT NULL,
-    "from_date" timestamp   NOT NULL,
-    "to_date" timestamp   NOT NULL
-);
-
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "birth_date" timestamp   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "gender" varchar   NOT NULL,
-    "hire_date" timestamp   NOT NULL
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    "from_date" timestamp   NOT NULL,
-    "to_date" timestamp   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "emp_no" int   NOT NULL,
-    "title" varchar   NOT NULL,
-    "from_date" timestamp   NOT NULL,
-    "to_date" timestamp   NOT NULL
-);
 
 SELECT * FROM "dept-emt"
 SELECT salaries.salary FROM "salaries"
